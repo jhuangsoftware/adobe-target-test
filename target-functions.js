1a2e938d3c028feb78f9823a516fe3cd262de98b
Jian Huang <jhuangsoftware@gmail.com>
2019-04-11 16:29:17 -0400

    Create target-functions.js

---
+++ target-functions.js
@@ -0,0 +1,9 @@
+function targetPageParams(){
+    return ['page=' + document.location.pathname,
+        'src=targetPageParams'];
+}
+// and/or
+function targetPageParamsAll(){
+    return ['page=' + document.location.pathname,
+        'src=targetPageParamsAll']
+}
